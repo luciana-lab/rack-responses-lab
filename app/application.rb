81cd04608ac903446b6cdc85fb4228aba8340dd2
Luciana Sugeta <lulusugeta@gmail.com>
2021-05-05 10:22:58 -0400

    commit tree c80345b7f2b5d6e2a542003e103c3d11968a3150

---
+++ app/application.rb
@@ -0,0 +1,17 @@
+require 'pry'
+class Application
+
+    def call(env)
+        resp = Rack::Response.new
+        time = Time.now
+        
+        # binding.pry
+        if  time.hour.between?(00, 12)
+            resp.write "Good Morning!"
+        else
+            resp.write "Good Afternoon!"
+        end
+
+    resp.finish
+    end
+end
